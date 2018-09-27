eaad7dc1a7c53528217b688d9d6369934e130952
Bluebird2000 <olamilekanahmad7@gmail.com>
2018-09-27 14:22:19 +0100

    send welcome message on start, establish first response to message

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ bot.on('start', () => {
     const params = {
         icon_emoji: ':componenthub:'
     };
-    bot.postMessageToChannel('general', 'Welcome! you can now search for any components in the store @componentbot', params);
+    bot.postMessageToChannel('general', 'Welcome! you can now search for any components/products in the store  By using @componentbot', params);
 });
 //Error handler;
 bot.on('error', (err) => console.log(err));
@@ -17,5 +17,21 @@ bot.on('message', (data) => {
     if (data.type !== 'message') {
         return;
     }
-    console.log(data);
+    handleMessage(data.text);
 });
+//Response to data
+function handleMessage(message) {
+    if (message.includes(' wattbank')) {
+        espModule();
+    }
+}
+function espModule() {
+    axios.get('https://xttreme.herokuapp.com/products')
+        .then(res => {
+            const searchedResult = res.data.data[0].product_description;
+            const params = {
+                icon_emoji: ':componenthub:'
+            };
+            bot.postMessageToChannel('general', `Searched result : ${searchedResult}`, params);
+        });
+}
